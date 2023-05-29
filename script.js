56aa5c3dbec5fdd48b755fa6f4beebec0f1e7124
Nazal-nihad <nazalnihad17@gmail.com>
2023-05-30 00:05:29 +0530

    changed base js

--- script.js
+++ script.js
@@ -1,83 +1,59 @@
 const keys = document.querySelectorAll('.key')
 const input = document.querySelector('.input')
 const output = document.querySelector('.output')
-const operator = document.querySelectorAll('.operator')
+const operators = document.querySelectorAll('.operator')
 let num1 = "";
 let num2 = "";
-let total;
-
-let a=0;
+let operator = "";
 
 keys.forEach(element => {
     element.addEventListener("click", () => {
         input.textContent += element.textContent;
 
-        if (element.textContent === "+") {
-            if (num2 != "") {
-                total = num1 + parseInt(num2);
-                total = parseInt(total);
-                console.log("total " + total)
-                output.textContent = total;
-                input.textContent = total;
-                input.textContent+=" + "
-            num1 = total;
-            }
-            num2 = "0";
-        }
-
-        if (element.textContent === "-") {
-            if (num2 != "") {
-                total = num1 - parseInt(num2);
-                total = parseInt(total);
-                console.log("total " + total)
-                output.textContent = total;
-                input.textContent = total;
-                input.textContent+=" - "
-            num1 = total;
-            }
-            num2 = "0";
-        }
-
-        // if (element.textContent === "x") {
-        //     if (num2 != "") {
-        //         total = parseInt(num1) * parseInt(num2);
-        //         total = parseInt(total);
-        //         console.log("total " + total)
-        //         output.textContent = total;
-        //         input.textContent = total;
-        //         input.textContent+=" + "
-        //     num1 = total;
-        //     }
-        //     num2 = "1";
-        // }
-
-        if (element.textContent === "/") {
-            if (num2 != "") {
-                total = num1 / parseInt(num2);
-                total = parseInt(total);
-                console.log("total " + total)
-                output.textContent = total;
-                input.textContent = total;
-                input.textContent+=" + "
-            num1 = total;
-            }
-            num2 = "0";
-        }
-
-
-        else if (num2 === "") {
+        if (operator === "") {
             num1 += element.textContent;
-            num1 = parseInt(num1);
         }
         else {
-            if (num2 === "1") {
-                num2 -= "1";
-            }
             num2 += element.textContent;
-            num2 = parseInt(num2);
         }
-        console.log("1",num1);
-        console.log("2",num2);
-    });    
-});
+    })
+})
 
+operators.forEach(element => {
+    element.addEventListener("click", () => {
+        operator = element.value;
+        // input.textContent += operator;
+
+        if (num1 !== "" && num2 !== "") {
+            calculate();
+        }
+    })
+})
+
+function calculate() {
+    let result;
+
+    switch (operator) {
+      case "+":
+        result = parseInt(num1) + parseInt(num2);
+        break;
+      case "-":
+        result = parseInt(num1) - parseInt(num2);
+        break;
+      case "*":
+        result = parseInt(num1) * parseInt(num2);
+        break;
+      case "/":
+        result = parseInt(num1) / parseInt(num2);
+        break;
+      default:
+        return;
+    }
+  
+    output.textContent = result;
+    input.textContent = result;
+  
+    num1 = result;
+    num2 = "";
+    operator = "";
+}
